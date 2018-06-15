70148baafd1d8c7ee6ed62aee94415866ca7f288
dhavalbagada <dhavalbagada@gmail.com>
2018-06-15 23:11:56 +0530

    Added Index.php & classic.js

---
+++ index.php
@@ -0,0 +1,127 @@
+<?php require_once("header.php");
+?>
+	<!--start-albums-->
+	 <div class="clearfix"></div>
+  <div class="albums">
+	<div class="container">
+		<div class="album-info">
+			<div class="col-md-3 album">
+			     <div class="album-box">
+				   <h4>Music</h4>
+				    <h5>For all Ages</h5>
+				 </div>
+			</div>
+			<div class="col-md-3 album">
+			     <div class="album-box">
+				   <h4>ALBUMS</h4>
+				    <h5>For all Ages</h5>
+				 </div>
+			</div>
+			<div class="col-md-3 album">
+			     <div class="album-box">
+				   <h4>SONGS</h4>
+				    <h5>For all Ages</h5>
+				 </div>
+			</div>
+			<div class="col-md-3 album lost">
+			     <div class="album-box">
+				   <h4>VIDEOS</h4>
+				    <h5>For all Ages</h5>
+				 </div>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+			<div class="music-info">
+				<div class="col-md-4 music-grids">
+					<figure class="effect-bubba">
+						<img src="images/m1.jpg" alt=""/>
+						<figcaption>
+							<h4>MUSIC ALBUM</h4>
+							<p>In sit amet sapien eros Integer in tincidunt labore et dolore magna aliqua</p>	
+						</figcaption>			
+					</figure>		
+				</div>
+				<div class="col-md-4 music-grids">
+					<figure class="effect-bubba">
+						<img src="images/m2.jpg" alt=""/>
+						<figcaption>
+							<h4>MUSIC ALBUM</h4>
+							<p>In sit amet sapien eros Integer in tincidunt labore et dolore magna aliqua</p>	
+						</figcaption>			
+					</figure>		
+				</div>
+				<div class="col-md-4 music-grids">
+					<figure class="effect-bubba">
+						<img src="images/m3.jpg" alt=""/>
+						<figcaption>
+							<h4>MUSIC ALBUM</h4>
+							<p>In sit amet sapien eros Integer  in tincidunt labore et dolore magna aliqua</p>							
+						</figcaption>			
+					</figure>		
+				</div>
+				<div class="clearfix"></div>
+			</div>
+				<div class="music-info two">
+				<div class="col-md-4 music-grids">
+					<figure class="effect-bubba">
+						<img src="images/m4.jpg" alt=""/>
+						<figcaption>
+							<h4>MUSIC ALBUM</h4>
+							<p>In sit amet sapien eros Integer in tincidunt labore et dolore magna aliqua</p>	
+						</figcaption>			
+					</figure>		
+				</div>
+				<div class="col-md-4 music-grids">
+					<figure class="effect-bubba">
+						<img src="images/m6.jpg" alt=""/>
+						<figcaption>
+							<h4>MUSIC ALBUM</h4>
+							<p>In sit amet sapien eros Integer in tincidunt labore et dolore magna aliqua</p>	
+						</figcaption>			
+					</figure>		
+				</div>
+				<div class="col-md-4 music-grids">
+					<figure class="effect-bubba">
+						<img src="images/m5.jpg" alt=""/>
+						<figcaption>
+							<h4>MUSIC ALBUM</h4>
+							<p>In sit amet sapien eros Integer  in tincidunt labore et dolore magna aliqua</p>							
+						</figcaption>			
+					</figure>		
+				</div>
+				<div class="clearfix"></div>
+			</div>
+				<div class="music-info two">
+				<div class="col-md-4 music-grids">
+					<figure class="effect-bubba">
+						<img src="images/m1.jpg" alt=""/>
+						<figcaption>
+							<h4>MUSIC ALBUM</h4>
+							<p>In sit amet sapien eros Integer in tincidunt labore et dolore magna aliqua</p>	
+						</figcaption>			
+					</figure>		
+				</div>
+				<div class="col-md-4 music-grids">
+					<figure class="effect-bubba">
+						<img src="images/m2.jpg" alt=""/>
+						<figcaption>
+							<h4>MUSIC ALBUM</h4>
+							<p>In sit amet sapien eros Integer in tincidunt labore et dolore magna aliqua</p>	
+						</figcaption>			
+					</figure>		
+				</div>
+				<div class="col-md-4 music-grids">
+					<figure class="effect-bubba">
+						<img src="images/m3.jpg" alt=""/>
+						<figcaption>
+							<h4>MUSIC ALBUM</h4>
+							<p>In sit amet sapien eros Integer  in tincidunt labore et dolore magna aliqua</p>							
+						</figcaption>			
+					</figure>		
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+	</div>
+	<!--//music-->
+<?php require_once("footer.php"); ?>
